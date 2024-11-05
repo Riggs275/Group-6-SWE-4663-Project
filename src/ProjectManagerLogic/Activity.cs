bebbd2e22142f7828ebb57b9dd840a4b05674474
Theary1123 <158534497+Theary1123@users.noreply.github.com>
2024-11-05 09:45:54 -0500

    Update Activity.cs

--- src/ProjectManagerLogic/Activity.cs
+++ src/ProjectManagerLogic/Activity.cs
@@ -1,5 +1,51 @@
 namespace ProjectManagerLogic;
 
-public class Activity {
-    
+using System;
+using System.Collections.Generic;
+
+class Activity
+{
+    public string ActivityName { get; set; }
+    public DateTime EstimatedTime { get; set; }
+    public List<TimeLogEntry> TotalTime { get; set; }
+
+    public Activity(string activityName, DateTime estimatedTime)
+    {
+        ActivityName = activityName;
+        EstimatedTime = estimatedTime;
+        TotalTime = new List<TimeLogEntry>();
+    }
+
+    public void AddTimeLog(TimeLogEntry timeLog)
+    {
+        TotalTime.Add(timeLog);
+    }
+}
+
+class ActivityManager
+{
+    private List<Activity> activities = new List<Activity>();
+
+    public void AddActivity(Activity activity)
+    {
+        activities.Add(activity);
+        Console.WriteLine($"Activity added: {activity.ActivityName}");
+    }
+
+    public bool RemoveActivity(string activityName)
+    {
+        var activity = activities.Find(a => a.ActivityName == activityName);
+        if (activity != null)
+        {
+            activities.Remove(activity);
+            Console.WriteLine($"Activity removed: {activityName}");
+            return true;
+        }
+        return false;
+    }
+
+    public List<Activity> ListActivities()
+    {
+        return new List<Activity>(activities);
+    }
 }
