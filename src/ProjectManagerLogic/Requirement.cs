f10fed1560df35a4a48d7cc03db2eeb1ad4cc53e
zachary Powell <sgeszachary@gmail.com>
2024-11-07 02:38:25 -0500

    setup values for classes

--- src/ProjectManagerLogic/Requirement.cs
+++ src/ProjectManagerLogic/Requirement.cs
@@ -1,5 +1,33 @@
 namespace ProjectManagerLogic;
 
-public class Requirement {
-    
+
+using System;
+using System.Collections.Generic;
+
+
+public class Requirement
+{
+
+    public string description {  get; set; }
+    public static int referenceNum { get; set; }
+    public Status requirementStatus;
+    public Priority importance;
+    public User owner;
+    public string errorMessage { get; set; }
+
+    public Requirement(string desc, Priority severity, User creator)
+    {
+        description = desc;
+        referenceNum = referenceNum++;
+        importance = severity;
+        owner = owner;
+        requirementStatus = Status.NotStarted;
+        errorMessage = "";
+    }
+
+    public string SetDescription(string newDescription)
+    {
+        description = newDescription;
+        return description;
+    }
 }
--- src/ProjectManagerLogic/User.cs
+++ src/ProjectManagerLogic/User.cs
@@ -1,30 +1,74 @@
 namespace ProjectManagerLogic;
 
-public class User {
+using System;
+using System.Collections.Generic;
 
+public class User
+{
     public string firstName { get; set; }
     public string middleInitial { get; set; }
     public string lastName { get; set; }
-    public bool isSuccess { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    public Role role;
+    public string roleDescription { get; set; }
     public string errorMessage { get; set; }
 
 
-    #region Not required (but helpful) Methods
 
-        public string GetFullName() {
+    public User()
+    {
 
-            string name = "";
-            
-            if (string.IsNullOrEmpty(middleInitial)) {
-                name = (firstName + " " + lastName);
-            }
-            else {
-                name = (firstName + " " + middleInitial + ". " + lastName);
-            }
+    }
+    //constructor
+    public User(string fN, string mI, string lN, Role job)
+    {
+        firstName = fN;
+        middleInitial = mI;
+        lastName = lN;
+        role = job;
+    }
 
-            return name;
-        }
+    //sets new firstName middle initial and lastName
+    public string setName(string fN, string mI, string lN)
+    {
+        string fullName;
+        firstName = fN;
+        middleInitial = mI;
+        lastName = lN;
+        fullName = fN + " " + mI + " " + lN;
+        return fullName;
+    }
+
+    public void login(string username, string password)
+    {
+        //compare username with password in database and alter a variable to being logged in state
+    }
+    public void logout()
+    {
+        //sets current state to being a logged out state
+    }
 
 
-    #endregion
+    //sets the current user's role description to a new value
+    public string setRoleDescription(string newRoleDescription)
+    {
+        roleDescription = newRoleDescription;
+        return roleDescription;
+    }
+
+
+    public List<User> getAllUsers()
+    {
+        List<User> listOfUsers = new List<User>();
+        //line connecting to database to run through and grab all of the users should be in a different area than in user class
+        return listOfUsers;
+    }
+
+    public User getUser(string username)
+    {
+        User foundUser = new User();
+        //line connecting to database to run through and search for a user based on the input username, should also be in the database connecting class
+        return foundUser;
+    }
 }
