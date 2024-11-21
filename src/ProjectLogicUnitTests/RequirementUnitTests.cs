cbfa029e1d4be4d07b59ac9d6d69bd647f55e4be
Ian Riggins <iriggins2003@gmail.com>
2024-11-21 14:52:12 -0500

    Added Unit Tests for Requirement and User for coverage

--- src/ProjectLogicUnitTests/RequirementUnitTests.cs
+++ src/ProjectLogicUnitTests/RequirementUnitTests.cs
@@ -4,33 +4,9 @@ namespace ProjectLogicUnitTests;
 using ProjectManagerLogic;
 
 public class RequirementUnitTests {
+    
     [Test]
-    public void SetRequirementsDescription_NewRequirementDescriptionSetCorrectly_ReturnsNewDescription()
-    {
-        //arrange
-        Requirement newRequirement = new Requirement();
-        string expectedDescription="Test description";
-        //act
-        string resultDescription = newRequirement.SetDescription("Test description");
-        //assert
-        Assert.That(resultDescription.Equals(expectedDescription));
-    }
-
-
-    [Test]
-    public void SetRequirementStatus_StatusChangedCorrectly_ReturnsConfirmation()
-    {
-        //arrange
-        Requirement newRequirement = new Requirement();
-        string expectedConfirm= "Status successfully changed!";
-        //act
-        string confirmation=newRequirement.setRequirementStatus("onhold");
-        //assert
-        Assert.That(confirmation.Equals(expectedConfirm));
-    }
-    [Test]
-    public void SetRequirementPriority_PriorityChangedCorrectly_ReturnsConfirmation()
-    {
+    public void SetRequirementPriority_PriorityChangedCorrectly_ReturnsConfirmation() {
         //arrange
         Requirement newRequirement = new Requirement();
         string expectedConfirm = "Priority successfully changed!";
@@ -38,6 +14,215 @@ public class RequirementUnitTests {
         string confirmation = newRequirement.setImportance("low") ;
         //assert
         Assert.That(confirmation.Equals(expectedConfirm));
+    }
+    
+    [Test]
+    public void SetRequirementStatus_Not_Started_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "Not Started";
+        const string expectedString = "Status successfully changed!";
+        const Status expectedStatus = Status.NotStarted;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setRequirementStatus(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.requirementStatus == expectedStatus);
+    }
+    
+    [Test]
+    public void SetRequirementStatus_in_progress_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "in progress";
+        const string expectedString = "Status successfully changed!";
+        const Status expectedStatus = Status.InProgress;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setRequirementStatus(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.requirementStatus == expectedStatus);
+    }
+    
+    [Test]
+    public void SetRequirementStatus_ON_HOLD_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "ON HOLD";
+        const string expectedString = "Status successfully changed!";
+        const Status expectedStatus = Status.OnHold;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setRequirementStatus(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.requirementStatus == expectedStatus);
+    }
+    
+    [Test]
+    public void SetRequirementStatus_coMpleTEd_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "coMpleTEd";
+        const string expectedString = "Status successfully changed!";
+        const Status expectedStatus = Status.Completed;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setRequirementStatus(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.requirementStatus == expectedStatus);
+    }
+    
+    [Test]
+    public void SetRequirementStatus_Mitigated_ReturnsError() {
+        
+        // Arrange
+        const string input = "Mitigated";
+        const string expectedString = "That is not a valid status";
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setRequirementStatus(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+    }
+    
+    [Test]
+    public void SetImportance_CRITICAL_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "CRITICAL";
+        const string expectedString = "Priority successfully changed!";
+        const Priority expectedImportance = Priority.Critical;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setImportance(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.importance == expectedImportance);
+    }
+    
+    [Test]
+    public void SetImportance_high_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "high";
+        const string expectedString = "Priority successfully changed!";
+        const Priority expectedImportance = Priority.High;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setImportance(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.importance == expectedImportance);
+    }
+    
+    [Test]
+    public void SetImportance_meDIum_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "meDIum";
+        const string expectedString = "Priority successfully changed!";
+        const Priority expectedImportance = Priority.Medium;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setImportance(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.importance == expectedImportance);
+    }
+    
+    [Test]
+    public void SetImportance_LoW_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "LoW";
+        const string expectedString = "Priority successfully changed!";
+        const Priority expectedImportance = Priority.Low;
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setImportance(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testRequirement.importance == expectedImportance);
+    }
+    
+    [Test]
+    public void SetImportance_Not_Important_ReturnsError() {
+        
+        // Arrange
+        const string input = "Not Important";
+        const string expected = "That is not a valid Priority";
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.setImportance(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+    
+    [Test]
+    public void SetDescription_ProperValueDescription_ReturnsSuccess() {
+        
+        // Arrange
+        const string description = "This is a description.";
+        const string expected = "Description saved successfully!";
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.SetDescription(description);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+    
+    [Test]
+    public void SetDescription_ExceededCharacterLimitDescription_ReturnsError() {
+        
+        // Arrange
+        const string description = ("dbkpSHN4RcKxSh6k1aJ3SYJp8Aru2pYSAFijLqmPiYZQXM8xhXJpRXhcRDBzd1b8nXZY4TA4f45FzmU" +
+                                    "h4nfaeXKB8UhBU49qdr5AKvjFmDA6ZJNByDkRuFC8k0TU3quijQL5CeQ9AV9tUTYq5Bp94MSdYvjDGR" +
+                                    "ZF3NUArU2DEuQhiaS6TjUHbcbTE06B8URujPPYcau9VcqWumJbZmT4GVvhSgUau11WzdJBjGXjPt56N" +
+                                    "XmJmGAEBdc56NM2hvyp6NCYKarwwMNN1LUVdLRBrGMTfL8vWtbymyQ9DCS1WN60P48YFy9w2A9LvxqC" +
+                                    "QVzy9AKYBjzE6YYvryMQaV9JtEBEKfv1YcfCqGbzA8Ypr5WEqDwLZk5mWgikNJj0Ra60hLBMwC4mR1Z" +
+                                    "M2gUJwj6GvPQ90Cvhdidgr9JCV95GRz1vG4SZxCAWgXyNpuXhrccmcS07DHAnFWfwTrHVGBmYPg1aFf" +
+                                    "7n1kKxcLTQcqC85xYzhzfWRvkNjy6WnK9idiGmr7BPmgePrayK2Pbeut26eDwp64KtPf3LxHvcdmvWT" +
+                                    "zXv1VJ7yBNJFEL52T6EuiQXedndTvvY22waNqpW1Wc75RnUBEUCFLawkENhqSTBxwVQyE8XPpDVbKET" +
+                                    "nfFfPtaDakap1SqM3MLS3vTB8ChWLnenQjyLTHrJ6GzVemU2c8L9L3MShvKjMk1eptcKxug32QvYxdx" +
+                                    "Nba2nSpyzpmuATSp7jWAvVGTSh8D65gR4BGXvabepxjmSZy0ajZzfrjUEgHCJpvMNygbybx35wYWRxx" +
+                                    "8AUPPf5a7GnLxbhFt7TPCMutxK7gMDXSktHdXhKrc6AF8mW0Hwqxayc0PcPyD8St3qrygm0k4Y5vBC2" +
+                                    "KgdKyPEyXzajuJxtytBQFaePaVDa0gg9L7F3J5QXLF2bjPvBKJVGcYYTHMPq8xNrBgQUVvBu6AiebeX" +
+                                    "U42Jm8iJUhKjwXmD2Ar1n5XnYNKvDF6vX6E7Hxu1FWJTpRHKTbeM8");
+        // Randomly generated string of size 1001
+        const string expected = "Description length cannot exceed 1,000 characters!";
+        Requirement testRequirement = new Requirement();
+        
+        // Act
+        string result = testRequirement.SetDescription(description);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
     }
 
 }
--- src/ProjectLogicUnitTests/UserUnitTests.cs
+++ src/ProjectLogicUnitTests/UserUnitTests.cs
@@ -2,104 +2,304 @@ namespace ProjectLogicUnitTests;
 using ProjectManagerLogic;
 
 public class UserUnitTests {
+    
     [Test]
-    public void UserConstructor_UserIsConstructedCorrectly_ReturnsInitializedFirstName()
-    {
+    public void UserConstructor_UserIsConstructedCorrectly_ReturnsInitializedFirstName() {
+        
         //arrange
         User newUser=new User("Zachary","B","Powell",Role.Developer);
         string expectedName = "Zachary";
+        
         //act
         string resultName = newUser.firstName;
+        
         //assert
         Assert.That(resultName.Equals(expectedName));
     }
 
     [Test]
-    public void SetName_UserNameIsSetCorrectly_FullNameIsReturnedOnSetNameMethod()
-    {
-        //arrange
-        User newUser = new User();
-        string expectedName = "Zachary B Powell";
-
-        //act
-        string resultFullName=newUser.setFullName("Zachary", "B", "Powell");
-        //assert
-        Assert.That(resultFullName.Equals(expectedName));
-    }
-
-
-    [Test]
-    public void SetRoleDescription_RoleDescriptionUpdatedToNewDescription_ReturnsNewDescription()
-    {
-        //arrange
-        User newUser= new User();
-        string ExpectedDescription = "Test description";
-
-        //act
-        string resultDescription = newUser.setRoleDescription("Test description");
-
-        //assert
-        Assert.That(resultDescription.Equals(ExpectedDescription));
-    }
-
-
-
-    [Test]
-    public void SetFullName_FullNameSetCorrectly_ReturnsFullName()
-    {
+    public void SetFullName_FullNameSetCorrectly_ReturnsFullName() {
+        
         //arrange
         User newUser = new User("Zachary","B","Powell",Role.Developer);
         string expectedName = "Zachary B. Powell";
+        
         //act
         string resultName = newUser.GetFullName();
-        //assert
-        Assert.That(resultName.Equals(expectedName));
-    }
-    [Test]
-    public void SetFirstName_FirstNameSetCorrectly_ReturnsFirstName()
-    {
-        //arrange
-        User newUser=new User();
-        string expectedName = "Zachary";
-        //act
-        string resultName = newUser.setFirstName("Zachary");
+        
         //assert
         Assert.That(resultName.Equals(expectedName));
     }
 
     [Test]
-    public void SetMiddleInitialMiddleInitialSetCorrectly_ReturnsMiddleInitial()
-    {
+    public void SetNewRole_RoleChangedToInput_ReturnsRoleSetCorrectly() {
+        
         //arrange
         User newUser = new User();
-        string expectedName = "B";
-        //act
-        string resultName=newUser.setMiddleInitial("B");
-        //assert
-        Assert.That(resultName.Equals(expectedName));
-    }
-
-    [Test]
-    public void SetLastName_LastNameSetCorrectly_ReturnsLastName()
-    {
-        //arrange
-        User newUser = new User();
-        string expectedName = "Powell";
-        //act
-        string resultName = newUser.setLastName("Powell");
-        //assert
-        Assert.That(resultName.Equals(expectedName));
-    }
-
-    [Test]
-    public void SetNewRole_RoleChangedToInput_ReturnsRoleSetCorrectly()
-    {
-        //arrange
-        User newUser = new User();
-        string expectedString = "Role set Correctly!";
+        string expectedString = "Role set successfully!";
+        
         //act
         string resultString=newUser.setRole("Developer");
+        
         //assert
         Assert.That(resultString.Equals(expectedString));
+    }
+
+    [Test]
+    public void SetFirstName_ProperValueName_ReturnsSuccess() {
+        
+        // Arrange
+        const string expected = "User first name set successfully!";
+        User testUser = new User();
+        string input = "Izuku";
+        
+        // Act
+        string result = testUser.setFirstName(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void SetFirstName_ExceededCharacterLimit_ReturnsError() {
+        
+        // Arrange
+        const string expected = "New first name is too long, max length is 50 characters";
+        User testUser = new User();
+        string input = "All-Out Final Battle Super Saiyan Blue Goku & Super Saiyan Blue Vegeta";
+        
+        // Act
+        string result = testUser.setFirstName(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+    
+    [Test]
+    public void SetMiddleInitial_ProperValueName_ReturnsSuccess() {
+        
+        // Arrange
+        const string expected = "User middle initial set successfully!";
+        User testUser = new User();
+        string input = "J";
+        
+        // Act
+        string result = testUser.setMiddleInitial(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void SetMiddleInitial_ExceededCharacterLimit_ReturnsError() {
+        
+        // Arrange
+        const string expected = "New middle initial is too long, max length is 2 characters";
+        User testUser = new User();
+        string input = "Three";
+        
+        // Act
+        string result = testUser.setMiddleInitial(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+    
+    [Test]
+    public void SetLastName_ProperValueName_ReturnsSuccess() {
+        
+        // Arrange
+        const string expected = "User last name set successfully!";
+        User testUser = new User();
+        string input = "Midoriya";
+        
+        // Act
+        string result = testUser.setLastName(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void SetLastName_ExceededCharacterLimit_ReturnsError() {
+        
+        // Arrange
+        const string expected = "New last name is too long, max length is 50 characters";
+        User testUser = new User();
+        string input = "We Saiyans have no limits! Let's charge together at full power!";
+        
+        // Act
+        string result = testUser.setLastName(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+    
+    [Test]
+    public void SetRole_Lead_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "Lead";
+        const string expectedString = "Role set successfully!";
+        const string expectedDescription = ("Responsible for overseeing the successful execution of project. " +
+                                           "Makes high-level decisions, manages resources, and communicates " +
+                                           "progress to higher management or clients.");
+        const Role expectedRole = Role.Lead;
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testUser.roleDescription.Equals(expectedDescription));
+        Assert.That(testUser.userRole == expectedRole);
+    }
+    
+    [Test]
+    public void SetRole_developer_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "developer";
+        const string expectedString = "Role set successfully!";
+        const string expectedDescription = ("Responsible for writing and maintaining code for the project. " +
+                                            "Works closely with project manager and other team members to " +
+                                            "ensure the project's success. Stays updated with the latest " +
+                                            "technologies and best practices to ensure the quality and " +
+                                            "efficiency of the project.");
+        const Role expectedRole = Role.Developer;
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testUser.roleDescription.Equals(expectedDescription));
+        Assert.That(testUser.userRole == expectedRole);
+    }
+    
+    [Test]
+    public void SetRole_cODe_rEVIEWEr_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "cODe rEVIEWEr";
+        const string expectedString = "Role set successfully!";
+        const string expectedDescription = ("Reviews the code written by developers to ensure it is of high " +
+                                            "quality, adheres to best practices, and meets the project’s " +
+                                            "standards.");
+        const Role expectedRole = Role.CodeReviewer;
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testUser.roleDescription.Equals(expectedDescription));
+        Assert.That(testUser.userRole == expectedRole);
+    }
+    
+    [Test]
+    public void SetRole_TesteR_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "TesteR";
+        const string expectedString = "Role set successfully!";
+        const string expectedDescription = ("Responsible for ensuring that the project meets all requirements " +
+                                            "and functions as intended before it is released.");
+        const Role expectedRole = Role.Tester;
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testUser.roleDescription.Equals(expectedDescription));
+        Assert.That(testUser.userRole == expectedRole);
+    }
+    
+    [Test]
+    public void SetRole_sTAKEholdER_ReturnsSuccess() {
+        
+        // Arrange
+        const string input = "sTAKEholdER";
+        const string expectedString = "Role set successfully!";
+        const string expectedDescription = ("Responsible for defining the goals and requirements of the project. " +
+                                            "May provide feedback during various phases of development, make " +
+                                            "decisions about the direction of the project, and help resolve issues.");
+        const Role expectedRole = Role.Stakeholder;
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expectedString));
+        Assert.That(testUser.roleDescription.Equals(expectedDescription));
+        Assert.That(testUser.userRole == expectedRole);
+    }
+    
+    [Test]
+    public void SetRole_User_ReturnsError() {
+        
+        // Arrange
+        const string input = "User";
+        const string expected = "That is not a valid role";
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.setRole(input);
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void GetFullName_NoMiddleInitial_ReturnsFirstandLast() {
+        
+        // Arrange
+        const string expected = "Spongebob Squarepants";
+        User testUser = new User();
+        testUser.setFirstName("Spongebob");
+        testUser.setLastName("Squarepants");
+        
+        // Act
+        string result = testUser.GetFullName();
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void GetFullName_WithMiddleInitial_ReturnsFirstMiddleandLast() {
+        
+        // Arrange
+        const string expected = "Sheldon J. Plankton";
+        User testUser = new User();
+        testUser.setFirstName("Sheldon");
+        testUser.setMiddleInitial("J");
+        testUser.setLastName("Plankton");
+        
+        // Act
+        string result = testUser.GetFullName();
+        
+        // Assert
+        Assert.That(result.Equals(expected));
+    }
+
+    [Test]
+    public void GetFullName_NoName_ReturnsError() {
+        
+        // Arrange
+        const string expected = "First and/or last name isn't set";
+        User testUser = new User();
+        
+        // Act
+        string result = testUser.GetFullName();
+        
+        // Assert
+        Assert.That(result.Equals(expected));
     }
 }
