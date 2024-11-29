2d68723ec4fc818665289efad86dd152436fcbf2
Ian Riggins <iriggins2003@gmail.com>
2024-11-28 23:51:39 -0500

    WebApp Project & Database Added

---
+++ src/ProjectManagerWebApp/Program.cs
@@ -0,0 +1,34 @@
+using ProjectManagerWebApp.Components;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagerLogic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register DbContext with MySQL provider
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
