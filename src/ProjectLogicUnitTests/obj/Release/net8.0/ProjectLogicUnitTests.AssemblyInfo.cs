c38be94825197fe09493da32826d0efe09133472
Zackiskip <sgeszachary@gmail.com>
2024-12-01 00:06:11 -0500

    nav bar fixed based on monte

--- src/ProjectLogicUnitTests/obj/Release/net8.0/ProjectLogicUnitTests.AssemblyInfo.cs
+++ src/ProjectLogicUnitTests/obj/Release/net8.0/ProjectLogicUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectLogicUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930fcbd349df9a0f72682235000ab1578f942874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d68723ec4fc818665289efad86dd152436fcbf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectLogicUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectLogicUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ProjectManagerLogic/obj/Release/net8.0/ProjectManagerLogic.AssemblyInfo.cs
+++ src/ProjectManagerLogic/obj/Release/net8.0/ProjectManagerLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagerLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930fcbd349df9a0f72682235000ab1578f942874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d68723ec4fc818665289efad86dd152436fcbf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagerLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagerLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
